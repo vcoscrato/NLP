df4094089df555ea0ca14d8ec4345a1414a08110
victor <vcoscrato@gmail.com>
2018-06-17 04:09:09 -0300

    app deploy

--- Trabalho futebol.py
+++ Trabalho futebol.py
@@ -1,30 +1,41 @@
-import numpy as np
-import pandas as pd
+from numpy import logical_and
+from pandas import DataFrame, concat, read_csv
 from nltk import word_tokenize, pos_tag_sents
 from collections import Counter
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.model_selection import cross_val_predict, LeaveOneOut
-import pickle
+from pickle import dump
 
-data = pd.read_csv('Anotacoes2.csv', encoding='ISO-8859-1')
+data = read_csv('Anotacoes2.csv', encoding='ISO-8859-1')
 data['tag'] = pos_tag_sents(data['Sentença'].apply(word_tokenize).tolist(), lang='pt')
 counts = []
 for k in range(len(data)):
     counts.append(Counter([j for i, j in data['tag'][k]]))
-dmm = pd.DataFrame(counts).fillna(0)
+dmm = DataFrame(counts).fillna(0)
 
 vec = CountVectorizer(min_df=0.01, max_df=0.5)
-dtm = pd.DataFrame(vec.fit_transform(data['Sentença'].values).toarray(), columns=vec.get_feature_names())
+dtm = DataFrame(vec.fit_transform(data['Sentença'].values).toarray(), columns=vec.get_feature_names())
 
-x = pd.concat([dtm, dmm], axis=1)
+x = concat([dtm, dmm], axis=1)
 y = data['Classe']
 
 classifier = GradientBoostingClassifier()
-cv = cross_val_predict(classifier, x, y, cv=LeaveOneOut())
 
+cv = cross_val_predict(classifier, x, y, cv=LeaveOneOut())
+acuracias = [sum(cv == y)/len(y),
+             sum(logical_and(cv == 'F', y == 'F')) / sum(y == 'F'),
+             sum(logical_and(cv == 'NF', y == 'NF')) / sum(y == 'NF')]
+with open('acuracias.pkl', 'wb') as f:
+    dump(acuracias, f)
 
 classifier.fit(x, y)
 
 with open('classifier.pkl', 'wb') as f:
-    pickle.dump(classifier, f, 2)
+    dump(classifier, f)
+
+with open('tokens.pkl', 'wb') as f:
+    dump(list(dtm), f)
+
+with open('classes.pkl', 'wb') as f:
+    dump(list(dmm), f)
--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, request, render_template
-import pickle
-import numpy as np
+from pickle import load
+from sklearn.feature_extraction.text import CountVectorizer
+from pandas import DataFrame, concat
+from nltk import word_tokenize, pos_tag_sents
+from collections import Counter
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+
+    with open('acuracias.pkl', 'rb') as f:
+        ac = load(f)
+
+    return render_template('home.html', total=ac[0], f=ac[1], nf=ac[2])
 
 
 @app.route('/predict', methods=['POST'])
@@ -15,19 +22,34 @@ def predict():
 
     result = request.form
 
-    # Prepare the feature vector for prediction
+    with open('tokens.pkl', 'rb') as f:
+        tokens = load(f)
 
-    prediction = str(result['texto'])
+    with open('classes.pkl', 'rb') as f:
+        classes = load(f)
 
+    text = str(result['texto'])
 
+    new_data = DataFrame(text.split('.'), columns=['Sentença'])
+    new_data['tag'] = pos_tag_sents(new_data['Sentença'].apply(word_tokenize).tolist(), lang='pt')
+    counts = []
+    for k in range(len(new_data)):
+        counts.append(Counter([j for i, j in new_data['tag'][k]]))
+    dmm = DataFrame(counts).fillna(0)
+    for i in range(len(classes)):
+        if classes[i] not in dmm:
+            dmm[classes[i]] = 0
 
+    vec = CountVectorizer(vocabulary=tokens)
+    dtm = DataFrame(vec.fit_transform(text.split('.')).toarray(), columns=vec.get_feature_names())
 
-    #pkl_file = open('classifier.pkl', 'rb')
-    #classifier = pickle.load(pkl_file)
-    #prediction = classifier.predict(new_vector)
+    with open('classifier.pkl', 'rb') as f:
+        classifier = load(f)
 
-    return render_template('result.html', prediction=prediction)
+    prediction = classifier.predict(concat([dtm, dmm], axis=1))
+    proportion = sum(prediction == 'F')/len(prediction)
 
+    return render_template('result.html', prediction=proportion)
 
 if __name__ == '__main__':
     app.run()
